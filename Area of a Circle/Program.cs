2dad9fe842a99eb4d3b04d76870bc9592191a4c9
Bolaji Oluwatofunmi Ashraf <Tofunmibolaji13@gmail.com>
2023-01-24 11:22:12 +0100

    Add project files.

---
+++ Area of a Circle/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Area_of_a_Circle
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // calculating the area of a circle
+            Console.WriteLine("Enter the value for radius");
+            const double pi = 3.14;
+            double radius = Convert.ToDouble(Console.ReadLine());
+            double areaOfCircle = (pi * (radius * radius));
+            Console.WriteLine("The area of the Circle is {0}", areaOfCircle);
+        }
+    }
+}
